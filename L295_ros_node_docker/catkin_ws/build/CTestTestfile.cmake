e8c755fcb16c19c41a5c124324c361ae822dca1b
Joshua Frank <frankjoshua@gmail.com>
2016-07-31 06:37:49 +0000

    Added ros package and run.sh

---
+++ L295_ros_node_docker/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/gobbit/L295_ros_node_docker/catkin_ws/src
+# Build directory: /home/pi/gobbit/L295_ros_node_docker/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ L295_ros_node_docker/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /opt/ros/indigo/setup.bash
+source ./catkin_ws/devel/setup.bash
+roslaunch l298n.launch
