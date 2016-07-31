efa31177e225a201bcd06979c3d6081684e7b7e9
Joshua Frank <frankjoshua@gmail.com>
2016-07-31 00:08:45 +0000

    Added l298n node

--- L295_ros_node_docker/l298n_node.py
+++ L295_ros_node_docker/l298n_node.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+import L298NHBridge as HBridge
+from geometry_msgs.msg import Twist
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+def callback(msg):
+    dx = msg.linear.x
+    dr = msg.angular.z
+    w = 0.2
+    right = 1.0 * dx + dr * w / 2
+    left = 1.0 * dx - dr * w / 2
+    HBridge.setMotorLeft(left)
+    HBridge.setMotorRight(right)
     
 def listener():
 
@@ -12,9 +19,9 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('l298n', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("/virtual_joystick/cmd_vel", Twist, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- L295_ros_node_docker/start.sh
+++ L295_ros_node_docker/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --cap-add SYS_RAWIO --device /dev/mem -v /home/pirate/gobbit:/home/pi/gobbit frankjoshua/gobbit-l298n-ros-node bash
+docker run -it --rm -p 11311:11311 --cap-add SYS_RAWIO --device /dev/mem -v /home/pirate/gobbit:/home/pi/gobbit frankjoshua/gobbit-l298n-ros-node bash
