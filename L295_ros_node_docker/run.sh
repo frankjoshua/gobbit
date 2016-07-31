e0bc1d383e837c1b19e9fce42fe4fa48932f41a4
Joshua Frank <frankjoshua@gmail.com>
2016-07-31 07:23:28 +0000

    Not auto starting

--- L295_ros_node_docker/run.sh
+++ L295_ros_node_docker/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chmod 777 /dev/mem
 source /opt/ros/indigo/setup.bash
 source /home/pi/gobbit/L295_ros_node_docker/catkin_ws/devel/setup.bash
 roslaunch /home/pi/gobbit/L295_ros_node_docker/l298n.launch
--- L295_ros_node_docker/start.sh
+++ L295_ros_node_docker/start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --privileged -it --rm -p 11311:11311 --cap-add SYS_RAWIO --device /dev/mem -v /home/pirate/gobbit:/home/pi/gobbit frankjoshua/gobbit-l298n-ros-node /home/pi/gobbit/L295_ros_node_docker/run.sh
+sudo chmod 777 /dev/mem
+sudo docker run --privileged -it --rm -p 11311:11311 --cap-add SYS_RAWIO --device /dev/mem -v /home/pirate/gobbit:/home/pi/gobbit frankjoshua/gobbit-l298n-ros-node bash 
+#/home/pi/gobbit/L295_ros_node_docker/run.sh
