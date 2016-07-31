3e8f39a801e385d1808092649785240fe4734873
Joshua Frank <frankjoshua@gmail.com>
2016-07-31 05:29:43 +0000

    Added launch file

--- L295_ros_node_docker/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/L295_ros_node_docker/catkin_ws/src
-# Build directory: /home/pi/L295_ros_node_docker/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(l298n)
--- L295_ros_node_docker/catkin_ws/build/l298n/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/L295_ros_node_docker/catkin_ws/src/l298n
-# Build directory: /home/pi/L295_ros_node_docker/catkin_ws/build/l298n
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
