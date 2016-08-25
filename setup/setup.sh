a76edbd5bca0c1145ac0ddfb01a5c1daae2b754a
Joshua Frank <frankjoshua@gmail.com>
2016-08-24 22:33:16 -0500

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#execute commands in script directory 
 cd "${0%/*}"
+
+#replace rc.local (probally not the best way) to run start.sh on boot
 sudo cp ./rc.local /etc/rc.local
+#copy start.sh to / (maybe this should go in /usr/bin instead but I wanted it to be visible)
 sudo cp ./start.sh /start.sh
 
 #Setup i2c
@@ -9,6 +13,9 @@ sudo echo i2c-bcm2708 >> /etc/modules
 sudo echo i2c-dev >> /etc/modules
 sudo echo dtparam=i2c1=on >> /boot/config.txt
 sudo echo dtparam=i2c_arm=on >> /boot/config.txt
+
+#run start.sh once so docker image will download
+sudo ./start.sh
 
 echo "Rebooting....."
 sudo reboot
