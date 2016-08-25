0a4fd81b6f285a9784c5b4a00421e7400596bacd
Joshua Frank <frankjoshua@gmail.com>
2016-08-25 01:59:53 +0000

    Improved setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+cd "${0%/*}"
 sudo cp ./rc.local /etc/rc.local
 sudo cp ./start.sh /start.sh
 
 #Setup i2c
-echo i2c-bcm2708 >> /etc/modules
-echo i2c-dev >> /etc/modules
-echo dtparam=i2c1=on >> /boot/config.txt
-echo dtparam=i2c_arm=on >> /boot/config.txt
+sudo echo i2c-bcm2708 >> /etc/modules
+sudo echo i2c-dev >> /etc/modules
+sudo echo dtparam=i2c1=on >> /boot/config.txt
+sudo echo dtparam=i2c_arm=on >> /boot/config.txt
 
+echo "Rebooting....."
 sudo reboot
-
