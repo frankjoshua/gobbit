e95554e5231b9b83f6fb99fdeb34d2300ac66646
Joshua Frank <frankjoshua@gmail.com>
2017-01-20 05:04:28 +0000

    Added line following pipe graphic

--- i2c_display_node_docker/start.sh
+++ i2c_display_node_docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod 666 /dev/i2c-1
-docker run --net host --privileged -v /dev:/dev -v /lib/modules:/lib/modules -v $PWD/node.py:/node.py -it frankjoshua/ros-rpi-oled-display
+#sudo chmod 666 /dev/i2c-1
+docker run --net host --privileged -v /dev:/dev -v /lib/modules:/lib/modules -v $PWD/node.py:/node.py -it frankjoshua/ros-rpi-oled-display $@
