cc9cfdfa532d21dfd3777171451f2aca99443f57
Joshua Frank <frankjoshua@gmail.com>
2017-01-23 04:46:05 +0000

    Added colors to start script

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+COLOR='\033[0;36m'
+COLOR2='\033[1;34m'
+NO_COLOR='\033[0;0m'
 ifconfig
 echo ""
 echo "To run in background use start.sh -d"
+echo -e $COLOR
 read -p "Enter device ip: " device_ip
+echo -e "${COLOR2}This is going to take a while......${NO_COLOR}"
 echo "#Created by script DO NOT EDIT" > ros.env
 echo "ROS_MASTER_URI=http://$device_ip:11311" >> ros.env
 echo "ROS_IP=$device_ip" >> ros.env
