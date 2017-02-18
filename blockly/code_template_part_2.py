c12d9a44719512e7218f92ed786ef7781f7424f7
Stuart Aldrich <artbot345@outlook.com>
2017-02-17 21:33:17 -0600

    actually ready for test

--- blockly/code_template_part_2.py
+++ blockly/code_template_part_2.py
@@ -16,7 +16,7 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('user_code_node', anonymous=True)
+    rospy.init_node('user_code_node', anonymous=False)
 	rospy.Subscriber('/line/filtered', Int32, line, queue_size=1)
 
     print "running uploaded blockly code"
--- blockly/update_code.php
+++ blockly/update_code.php
@@ -1,4 +1,5 @@
 <?php
 file_put_contents("user_code.py", $_POST["code"], LOCK_EX);
 system("cat code_template.py user_code.py > combined_code.py");
+system("python combined_code.py");
 ?>
