070054a62b937eb2159e61e0782dc1b71758a765
Joshua Frank <frankjoshua@gmail.com>
2017-02-19 01:34:01 -0600

    Revert "Master" (#3)

--- blockly/generators/python/move_gen.js
+++
@@ -1,7 +0,0 @@
-Blockly.Python['move'] = function(block) {
-  var number_twist_value = block.getFieldValue('twist value');
-  var value_twist_value = Blockly.Python.valueToCode(block, 'twist value', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
-  //var code = '...\n';
-  return number_twist_value;
-};
