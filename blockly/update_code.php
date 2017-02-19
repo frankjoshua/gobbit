83664e63a2edea8fc3805776c661ebc49347400c
Joshua Frank <frankjoshua@gmail.com>
2017-02-19 01:33:40 -0600

    Revert "Master"

--- blockly/update_code.php
+++ blockly/update_code.php
@@ -1,5 +1,3 @@
 <?php
-file_put_contents("user_code.py", $_POST["code"], LOCK_EX);
-system("cat code_template.py user_code.py > combined_code.py");
-system("python combined_code.py");
+file_put_contents("code.py", $_POST["code"], LOCK_EX);
 ?>
--- user_code_node/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -t frankjoshua/ros-rpi-line-follower .
-docker push frankjoshua/ros-rpi-line-follower
