91658c568a4b34e39f8d71bf1a6ab580e0349c9e
Joshua Frank <frankjoshua@gmail.com>
2017-03-06 01:30:38 +0000

    Updated description

--- description/start.sh
+++ description/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker rm -f description
-sudo docker run -it --name description -e ROS_IP=10.10.10.101 --net=host -v $PWD/description.launch:/description.launch -v $PWD/model.urdf.xacro:/model.urdf.xacro frankjoshua/ros-rpi-gobbit-description $@
+sudo docker run -it --name description -e ROS_IP=10.10.10.106 --net=host -v $PWD/description.launch:/description.launch -v $PWD/model.urdf.xacro:/model.urdf.xacro frankjoshua/ros-rpi-gobbit-description $@
