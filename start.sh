4d830c99a5d77ecce6f3201bf57a4a338e1d0e45
Joshua Frank <frankjoshua@gmail.com>
2017-03-12 03:37:12 +0000

    start.sh now uses wlan0 ip by default

--- start.sh
+++ start.sh
@@ -2,13 +2,12 @@
 COLOR='\033[0;36m'
 COLOR2='\033[1;34m'
 NO_COLOR='\033[0;0m'
-ifconfig
 echo ""
-echo "To run in background use start.sh -d"
 echo -e $COLOR
-read -p "Enter device ip: " device_ip
+#read -p "Enter device ip: " device_ip
+device_ip=$(ifconfig wlan0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 echo -e "${COLOR2}This is going to take a while......${NO_COLOR}"
 echo "#Created by script DO NOT EDIT" > ros.env
 echo "ROS_MASTER_URI=http://$device_ip:11311" >> ros.env
 echo "ROS_IP=$device_ip" >> ros.env
-docker-compose up $@
+docker-compose up -d
