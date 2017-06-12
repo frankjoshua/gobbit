d4b9e6a4b81e881c522ca6da68d3d48ca0367f68
Joshua Frank <josh@tesseractmobile.com>
2017-06-12 00:44:38 -0500

    Added xv_11 laser and hactor mapping

--- start.sh
+++ start.sh
@@ -6,8 +6,18 @@ COLOR2='\033[1;34m'
 NO_COLOR='\033[0;0m'
 echo ""
 echo -e $COLOR
-#read -p "Enter device ip: " device_ip
+#Get ip from wireless
 device_ip=$(ifconfig wlan0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+#If not found set from ethernet
+if [ -z "$device_ip" ]
+then
+  device_ip=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+fi
+#If still not set ask user
+if [ -z "$device_ip" ]
+then
+  read -p "Enter device ip: " device_ip
+fi
 echo -e "${COLOR2}This is going to take a while......${NO_COLOR}"
 echo "#Created by script DO NOT EDIT" > ros.env
 echo "ROS_MASTER_URI=http://$device_ip:11311" >> ros.env
---
+++ xv_11_laser/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -t frankjoshua/docker_ros_xv_11 .
+docker push frankjoshua/docker_ros_xv_11
