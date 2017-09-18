aef9102b69bd03442e83c8b093006743c5251b90
josh <josh@tesseractmobile.com>
2017-09-17 22:02:23 -0500

    Updated ros-diff-drive for x86

--- docker_ros_nodes/ros_diff_drive/build.sh
+++ docker_ros_nodes/ros_diff_drive/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-docker build -t frankjoshua/ros-diff-drive .
-docker push frankjoshua/ros-diff-drive
+ARCH=$(dpkg --print-architecture)
+echo $ARCH
+docker build -t frankjoshua/ros-diff-drive:$ARCH .
+docker push frankjoshua/ros-diff-drive:$ARCH
+manifest-tool push from-spec manifest.yaml
