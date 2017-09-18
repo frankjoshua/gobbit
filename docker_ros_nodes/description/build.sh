554426c674ddf7137e9b5c248ad8f9fce0282628
josh <josh@tesseractmobile.com>
2017-09-17 21:41:29 -0500

    Added docker manifest

--- docker_ros_nodes/description/build.sh
+++ docker_ros_nodes/description/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ARCH=$(dpkg --print-architecture)
 echo $ARCH
-docker build -t frankjoshua/ros-gobbit-description-$ARCH .
-docker push frankjoshua/ros-gobbit-description-$ARCH
+docker build -t frankjoshua/ros-gobbit-description:$ARCH .
+docker push frankjoshua/ros-gobbit-description:$ARCH
+manifest-tool push from-spec manifest.yaml
