e1f9d1b659c8bde431d445f5279d4e300533f651
josh <josh@tesseractmobile.com>
2017-09-17 19:50:50 -0500

    Make description node build on x86

--- docker_ros_nodes/description/build.sh
+++ docker_ros_nodes/description/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+ARCH='uname -m'
+echo $ARCH
 docker build -t frankjoshua/ros-rpi-gobbit-description .
 docker push frankjoshua/ros-rpi-gobbit-description
--- docker_ros_nodes/description/start.sh
+++ docker_ros_nodes/description/start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+#Get ip from wireless
+device_ip=$(ifconfig wlan0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+#If not found set from ethernet
+if [ -z "$device_ip" ]
+then
+  device_ip=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+fi
+#If still not set ask user
+if [ -z "$device_ip" ]
+then
+  read -p "Enter device ip: " device_ip
+fi
 sudo docker rm -f description
-sudo docker run -it --name description -e ROS_IP=10.10.10.106 --net=host -v $PWD/description.launch:/description.launch -v $PWD/model.urdf.xacro:/model.urdf.xacro frankjoshua/ros-rpi-gobbit-description $@
+sudo docker run -it --name description -e ROS_IP=$device_ip --net=host -v $PWD/description.launch:/description.launch -v $PWD/model.urdf.xacro:/model.urdf.xacro frankjoshua/ros-rpi-gobbit-description $@
