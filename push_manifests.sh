dbc7dbf336e883c6d35be2cd5c4b81fc49041e4f
josh <josh@tesseractmobile.com>
2017-10-05 23:49:08 -0500

    Renamed directories to match images

---
+++ push_manifests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for d in ./docker_ros_nodes/*/ ; do
+file=$d/manifest.yaml
+if [ -f "$file" ]
+then
+dirname=$(basename $d)
+echo "manifest-tool push from-args \
+    --platforms linux/amd64,linux/arm64 \
+    --template frankjoshua/$dirname:ARCH \
+    --target frankjoshua/$dirname:latest"
+#manifest-tool push from-spec $file
+manifest-tool push from-args \
+    --platforms linux/amd64,linux/arm64 \
+    --template frankjoshua/$dirname:ARCH \
+    --target frankjoshua/$dirname:latest
+fi
+done
