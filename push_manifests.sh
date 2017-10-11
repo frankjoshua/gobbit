6ef4e552b48cd8416c752ec7da90cdeaac98280e
josh <josh@tesseractmobile.com>
2017-10-10 22:45:13 -0500

    Updated manifest push script

--- push_manifests.sh
+++ push_manifests.sh
@@ -6,7 +6,7 @@ if [ -f "$file" ]
 then
 dirname=$(basename $d)
 echo "manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm64 \
+    --platforms linux/amd64,linux/arm \
     --template frankjoshua/$dirname:ARCH \
     --target frankjoshua/$dirname:latest"
 #manifest-tool push from-spec $file
