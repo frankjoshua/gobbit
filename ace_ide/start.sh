268cc6dbd16f443ed99a0bef6f587cbd1e7ba3d9
Joshua Frank <josh@tesseractmobile.com>
2017-11-10 22:17:57 -0600

    Changed to arch

--- ace_ide/start.sh
+++ ace_ide/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#docker run --name rpi-c9-ide --restart unless-stopped -d -p 8181:8181 -v ~/gobbit:/workspace hwegge2/rpi-cloud9-ide node server.js -w/workspace --listen 0.0.0.0 -a :
-docker run --name rpi-c9-ide --restart unless-stopped -d -p 8181:8181 -v ~/gobbit:/workspace kdelfour/cloud9-docker node server.js -w/workspace --listen 0.0.0.0 -a :
+docker run --name rpi-c9-ide --restart unless-stopped -d -p 8181:8181 -v ~/gobbit:/workspace hwegge2/rpi-cloud9-ide node server.js -w/workspace --listen 0.0.0.0 -a :
+#docker run --name rpi-c9-ide --restart unless-stopped -d -p 8181:8181 -v ~/gobbit:/workspace kdelfour/cloud9-docker node server.js -w/workspace --listen 0.0.0.0 -a :
