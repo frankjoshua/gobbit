d19c4e2ded01795cc2c62ff0af113c21a740965e
josh <josh@tesseractmobile.com>
2018-05-08 00:28:32 -0500

    Added vel-muxer

--- docker_ros_nodes/ros-dnn-tracker/node.py
+++ docker_ros_nodes/ros-dnn-tracker/node.py
@@ -7,8 +7,9 @@ from geometry_msgs.msg import Twist
 from dnn_detect.msg import DetectedObjectArray
 
 #Motor controller topic
-motorPub = rospy.Publisher('/pocketbot/cmd_vel', Twist, queue_size=1)
+motorPub = rospy.Publisher('/cmd_vel/computer', Twist, queue_size=1)
 lastTurn = 0
+cmd = Twist()
 
 def newImage(msg):
     global lastTurn
@@ -16,25 +17,36 @@ def newImage(msg):
         #Sort by object size
         sortedObjects = sorted(msg.objects, key=lambda object: (object.x_max - object.x_min) * (object.y_max - object.y_min))
         #Sort by object type
-        sortedObjects.sort(key=lambda object: object.class_name)
+        sortedObjects.sort(key=lambda object: object.class_name, reverse=True)
         #Find center of object
         screen_width = 640
         screen_center = screen_width / 2
         xmin = sortedObjects[0].x_min
         xmax = sortedObjects[0].x_max
+        class_name = sortedObjects[0].class_name
         center = xmin + (xmax - xmin) / 2
         turn = (center - screen_center) / screen_width
-        if turn != lastTurn: 
+        if class_name == 'person' and turn != lastTurn: 
             #Turn towards object
             lastTurn = turn
-            rospy.logerr("Got dnn image %s", sortedObjects)
-            cmd = Twist()
-            cmd.angular.z = -turn
-            motorPub.publish(cmd)
+            cmd.angular.z = -turn * 1.25
+            cmd.linear.y = 0
+            if class_name == 'bottle':
+                cmd.linear.x = 0.5
+            elif class_name == 'person':
+                cmd.linear.x = 0.75
+            else:
+                cmd.linear.x = 0 #-0.2
+            #cmd.linear.x = 0
+            rospy.loginfo("Got dnn image %s", sortedObjects)    
         else:
             #Stop
-            motorPub.publish(Twist())
-    
+            #cmd.angular.z = cmd.angular.z * 0.5
+            cmd.angular.z = -lastTurn
+            #cmd.angular.z = 0
+            cmd.linear.y = 0
+            cmd.linear.x = 0
+        motorPub.publish(cmd)
 
 def listener():
     rospy.init_node('dnn_tracker', anonymous=False)
